5e4e9c48a9ed4de40ce442c0c3975779437c56fd
Rafael Medeiros <64844045+RafaellMedeiros@users.noreply.github.com>
2026-04-26 23:17:58 +0000

    feat: refactor LivroController and LivroRoutes for improved readability and functionality

--- express-mongo/src/controller/LivroController.js
+++ express-mongo/src/controller/LivroController.js
@@ -1,18 +1,59 @@
-import livros from '../models/livros.js'
+import livros from "../models/livros.js";
 
 class LivroController {
-    static listarLivros = async (req, res) => {
-        const listaLivros = await livros.find();
+  static listarLivros = async (req, res) => {
+    const listaLivros = await livros.find();
 
-        res.status(200).json(listaLivros)
-    }
+    res.status(200).json(listaLivros);
+  };
 
-    static criarLivro = async (req, res) => {
-        const { titulo, autor } = req.body
-        const livro = { titulo, autor }
-        livros.create(livro)
-        res.status(201).send('Livro adicionado com sucesso')
+  static criarLivro = async (req, res) => {
+    const { titulo, autor } = req.body;
+    const livro = { titulo, autor };
+    livros.create(livro);
+    res.status(201).send("Livro adicionado com sucesso");
+  };
+
+  static buscaLivroById = async (req, res) => {
+    const id = req.params.id;
+    try {
+      const livro = await livros.findById(id);
+      if (livro) {
+        res.status(200).send(livro);
+      } else {
+        res.status(404).send({ message: "Livro não encontrado." });
+      }
+    } catch (err) {
+      res
+        .status(400)
+        .send({ message: `${err.message} - Id do livro inválido.` });
     }
+  };
+
+  static atualizaLivro = async (req, res) => {
+    const id = req.params.id;
+    const livroAtualizado = req.body;
+
+    await livros.findByIdAndUpdate(id, { $set: livroAtualizado }, (err) => {
+      if (!err) {
+        res.status(200).send({ message: "Livro atualizado com sucesso." });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
+
+  static deletaLivro = async (req, res) => {
+    const id = req.params.id;
+
+    await livros.findByIdAndDelete(id, (err) => {
+      if (!err) {
+        res.status(200).send({ message: "Livro deletado com sucesso." });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  };
 }
 
-export default LivroController
+export default LivroController;
--- express-mongo/src/routes/LivroRoutes.js
+++ express-mongo/src/routes/LivroRoutes.js
@@ -1,9 +1,12 @@
-import LivroController from '../controller/LivroController.js'
-import express from 'express'
+import LivroController from "../controller/LivroController.js";
+import express from "express";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/livros', LivroController.listarLivros)
-router.post('/livro', LivroController.criarLivro)
+router.get("/livros", LivroController.listarLivros);
+router.post("/livro", LivroController.criarLivro);
+router.get("/livro/:id", LivroController.buscaLivroById);
+router.put("/livro/:id", LivroController.atualizaLivro);
+router.delete("/livro/:id", LivroController.deletaLivro);
 
-export default router
+export default router;
